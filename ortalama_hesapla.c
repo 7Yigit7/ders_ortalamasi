f199e76768fa282c126fc65ecb7904fbd5e179a9
7Yigit7 <yigitcitak.32@gmail.com>
2024-07-14 22:34:51 +0300

    Add files via upload

---
+++ ortalama_hesapla.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int mat = 0;
+	int bilisim = 0;
+	int turk = 0;
+	int tarih = 0;
+	int fen = 0;
+	int muz = 0;
+
+	printf("Matematik Notu: ");
+	scanf("%d",&mat);
+
+	printf("Bilişim notu: ");
+	scanf("%d",&bilisim);
+
+	printf("Türkçe dersi: ");
+	scanf("%d",&turk);
+
+	printf("Tarih: ");
+	scanf("%d",&tarih);
+
+	printf("Fen: ");
+	scanf("%d",&fen);
+
+	printf("Müzik: ");
+	scanf("%d",&muz);
+
+	int toplami = mat + bilisim + turk + tarih + fen + muz;
+	int ders_sayisi = 6;
+	int sonuc = toplami / ders_sayisi;
+
+	printf("Sonuç %d \n",sonuc);
+	return 0;
+}
